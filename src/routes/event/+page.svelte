51cb40781aed7aa3d39d948409557d59b44922d5
nav2924 <navi29012004@gmail.com>
2024-11-06 10:04:07 +0530

    Responsiveness added for event page

--- src/routes/event/+page.svelte
+++ src/routes/event/+page.svelte
@@ -1,39 +1,39 @@
-<script>
-    import "../../public/main.css";
-    import Nav from "../../component/Nav.svelte";
-    import Card from "../../component/Card.svelte";
-    let events = [{
-        "title": "School of Business and Management",
-        "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
-        "link" : "/event/businessandmanagement",
-    },{
-        "title": "School of Engineering",
-        "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
-        "link" : "/event/engineering",
-    },{
-        "title": "School of Psychology",
-        "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
-        "link" : "/event/pscyh",
-    },{
-        "title": "School of Architecture",
-        "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
-        "link" : "/event/arch",
-    }]
-</script>
-<div class="about-div">
-    <div class="bg-bubble"></div>
-    <Nav />
-    <div class="about-info">
-        <h1>
-            Our Events
-        </h1>
-        <p>
-            lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. 
-        </p>
-        <div class="about-grid">
-            {#each events as event}
-                <Card title={event.title} description={event.desc} link={event.link}  />
-            {/each}
+    <script>
+        import "../../public/main.css";
+        import Nav from "../../component/Nav.svelte";
+        import Card from "../../component/Card.svelte";
+        let events = [{
+            "title": "School of Business and Management",
+            "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
+            "link" : "/event/businessandmanagement",
+        },{
+            "title": "School of Engineering",
+            "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
+            "link" : "/event/engineering",
+        },{
+            "title": "School of Psychology",
+            "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
+            "link" : "/event/pscyh",
+        },{
+            "title": "School of Architecture",
+            "desc" : "lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. ",
+            "link" : "/event/arch",
+        }]
+    </script>
+    <div class="about-div">
+        <div class="bg-bubble"></div>
+        <Nav />
+        <div class="about-info">
+            <h1>
+                Our Events
+            </h1>
+            <p>
+                lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. lorem ipsum is a sample text for content display to show the content distribution for mockups in UI design. 
+            </p>
+            <div class="about-grid">
+                {#each events as event}
+                    <Card title={event.title} description={event.desc} link={event.link}  />
+                {/each}
+            </div>
         </div>
     </div>
-</div>
